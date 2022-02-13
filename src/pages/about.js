dbfc431a9caacbc6fe1794fb5d118f4608cf9c78
Nok <dive877@gmail.com>
2022-02-13 16:52:21 +0400

    Push to remote repo

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Page">
+      <p>So proud, this thing is already looking amazing</p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
