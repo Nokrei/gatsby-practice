a6be7012f317d6bb99ab7313f5bd6f3cc9288ba3
Nok <dive877@gmail.com>
2022-02-13 18:18:38 +0400

    Add blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,19 @@
 import * as React from "react";
 import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 import Layout from "../components/layout";
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
-        {data.allFile.nodes.map((node) => {
-          return <li key={node.name}>{node.name}</li>;
+      <ul style={{ padding: "0" }}>
+        {data.allMdx.nodes.map((node) => {
+          return (
+            <article key={node.id}>
+              <h2>{node.frontmatter.title}</h2>
+              <p>Posted {node.frontmatter.date}</p>
+              <MDXRenderer>{node.body}</MDXRenderer>
+            </article>
+          );
         })}
       </ul>
     </Layout>
@@ -14,9 +21,14 @@ const BlogPage = ({ data }) => {
 };
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
       nodes {
-        name
+        frontmatter {
+          title
+          date(formatString: "MMMM DD, YYYY")
+        }
+        id
+        body
       }
     }
   }
